a107c9a3f1c0857549329f2cadc0469e8af49137
shaoxyz <shwb95@163.com>
2026-08-22 10:10:01 +0800

    content: add 2026-08-22 BHP report

--- scripts/build-site.mjs
+++ scripts/build-site.mjs
@@ -41,7 +41,8 @@ const companyNames = {
   Tencent: "腾讯",
   HomeDepot: "家得宝",
   EsteeLauder: "雅诗兰黛集团",
-  Walmart: "沃尔玛"
+  Walmart: "沃尔玛",
+  BHP: "必和必拓"
 };
 
 const industries = {
@@ -55,7 +56,7 @@ const industries = {
   ProcterAndGamble: "日用消费品", McDonalds: "连锁餐饮",
   Nike: "运动鞋服", Toyota: "汽车制造", LVMH: "奢侈品集团",
   Tencent: "互联网平台与游戏", HomeDepot: "家居建材零售",
-  EsteeLauder: "高端美妆", Walmart: "综合零售"
+  EsteeLauder: "高端美妆", Walmart: "综合零售", BHP: "矿业与资源"
 };
 
 const clean = (value) => value
